03085ce93d5c4bffed919ad28757492a76d5a5ba
Carl Mylo <56451149+carlmylo@users.noreply.github.com>
2025-10-01 18:31:45 -0500

    figure this out later. public images don't seem to work.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://carlmylo.github.io/',
-  base: '/milohaxguides/',
+  base: '/',
     integrations: [starlight({
         title: 'MiloHax Guides',
           social: [
