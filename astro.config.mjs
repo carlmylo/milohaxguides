60c9e7e52dede1bed69c0280bea2c95842c38128
Carl Mylo <56451149+carlmylo@users.noreply.github.com>
2025-10-01 17:57:12 -0500

    Images and Astro update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://carlmylo.github.io/',
-  base: '/carlmylo.github.io/',
+  base: 'milohaxguides',
     integrations: [starlight({
         title: 'MiloHax Guides',
           social: [
                 { icon: 'seti:firefox', label: 'Website', href: 'https://milohax.org/' },
                 { icon: 'discord', label: 'Discord', href: 'https://discord.gg/milohax' },
                 { icon: 'github', label: 'GitHub', href: 'https://github.com/hmxmilohax' },
+                { icon: 'youtube', label: 'YouTube', href: 'https://www.youtube.com/@MiloHax' },
                 { icon: 'twitter', label: 'GitLab', href: 'https://twitter.com/hmxmilohax' },
 ],
   			customCss: ['./src/styles/custom.css'],
